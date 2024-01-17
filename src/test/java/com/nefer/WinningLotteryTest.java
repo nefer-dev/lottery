9266bc0d7bd520c36e05c510220f0c0c3d8371a7
cheolhakja <rebegin15@gmail.com>
2024-01-17 23:18:04 +0900

    test: a single first-place lottery ticket

---
+++ src/test/java/com/nefer/WinningLotteryTest.java
@@ -0,0 +1,24 @@
+package com.nefer;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningLotteryTest {
+
+    @Test
+    public void winTheFirstPlace() {
+        User user = new User("Judy", "C000000");
+        user.addLotteryTicket(new LotteryTicket("1", new ArrayList<>(List.of(1,2,3,4,5,6))));
+
+        ArrayList<Ranking> rankingList = user.getRankingList(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+
+        Assertions.assertThat(rankingList.size()).isEqualTo(1);
+        Assertions.assertThat(rankingList.get(0)).isEqualTo(Ranking.FIRST);
+
+
+    }
+
+}
