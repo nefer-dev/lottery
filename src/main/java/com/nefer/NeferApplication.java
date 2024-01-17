e1950623bb2fcc75431c492b933b09f9189b5e68
cheolhakja <rebegin15@gmail.com>
2024-01-17 17:56:42 +0900

    init

---
+++ src/main/java/com/nefer/NeferApplication.java
@@ -0,0 +1,13 @@
+package com.nefer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nefer/NeferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nefer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
