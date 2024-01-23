7cc363072639526a5414b96e3c8ee9e1b9ab550a
cheolhakja <rebegin15@gmail.com>
2024-01-23 15:38:08 +0900

    add: LotteryController

---
+++ src/main/java/com/nefer/LotteryController.java
@@ -0,0 +1,14 @@
+package com.nefer;
+
+public class LotteryController {
+
+    public void run() {
+        User user1 = new User("Saka", "C111111");
+        User user2 = new User("Nunez", "C222222");
+        User user3 = new User("CR7", "C333333");
+
+
+
+
+    }
+}
---
+++ src/main/java/com/nefer/LotteryLauncher.java
@@ -0,0 +1,8 @@
+package com.nefer;
+
+public class LotteryLauncher {
+    public static void main(String[] args) {
+        LotteryController lotteryController = new LotteryController();
+        lotteryController.run();
+    }
+}
