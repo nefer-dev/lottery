246edb4269098963e43674bdc1d48d36939a4b9c
cheolhakja <rebegin15@gmail.com>
2024-01-23 15:37:15 +0900

    test: second place stack

--- src/test/java/com/nefer/FinalWinnerRandomDrawTest.java
+++ src/test/java/com/nefer/FinalWinnerRandomDrawTest.java
@@ -15,11 +15,11 @@ public class FinalWinnerRandomDrawTest {
         User user = new User("aaa", "C333333");
         user.addLotteryTicket(new LotteryTicket("5", new ArrayList<>(List.of(1,2,3,4,5,6))));
         user.addLotteryTicket(new LotteryTicket("6", new ArrayList<>(List.of(1,2,3,4,5,6))));
-        user.addLotteryTicket(new LotteryTicket("7", new ArrayList<>(List.of(1,2,3,4,5,6))));
-        user.addLotteryTicket(new LotteryTicket("8", new ArrayList<>(List.of(1,2,3,4,5,6))));
-        user.addLotteryTicket(new LotteryTicket("9", new ArrayList<>(List.of(1,2,3,4,5,6))));
-        user.addLotteryTicket(new LotteryTicket("10", new ArrayList<>(List.of(1,2,3,4,5,6))));
-        user.addLotteryTicket(new LotteryTicket("11", new ArrayList<>(List.of(1,2,3,4,5,6))));
+        user.addLotteryTicket(new LotteryTicket("7", new ArrayList<>(List.of(1,2,3,4,5,7))));
+        user.addLotteryTicket(new LotteryTicket("8", new ArrayList<>(List.of(1,2,3,4,5,8))));
+        user.addLotteryTicket(new LotteryTicket("9", new ArrayList<>(List.of(1,2,3,4,6,10))));
+        user.addLotteryTicket(new LotteryTicket("10", new ArrayList<>(List.of(1,2,3,7,8,9))));
+        user.addLotteryTicket(new LotteryTicket("11", new ArrayList<>(List.of(1,2,3,10,9,8))));
 
 
         ArrayList<Ranking> rankingList = user.getRankingList(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
@@ -58,6 +58,50 @@ public class FinalWinnerRandomDrawTest {
         }
 
         user.setSecondPlaceWinningStack(remainder);
+    }
+
+    @Test
+    public void testSecondPlace() {
+        User user = new User("aaa", "C333333");
+        user.addLotteryTicket(new LotteryTicket("5", new ArrayList<>(List.of(1,2,3,4,5,7))));
+        user.addLotteryTicket(new LotteryTicket("6", new ArrayList<>(List.of(1,2,3,4,5,7))));
+        user.addLotteryTicket(new LotteryTicket("7", new ArrayList<>(List.of(1,2,3,4,5,7))));
+        user.addLotteryTicket(new LotteryTicket("8", new ArrayList<>(List.of(1,2,3,4,5,7))));
+        user.addLotteryTicket(new LotteryTicket("9", new ArrayList<>(List.of(1,2,3,4,5,7))));
+        user.addLotteryTicket(new LotteryTicket("10", new ArrayList<>(List.of(1,2,3,4,5,7))));
+        user.addLotteryTicket(new LotteryTicket("11", new ArrayList<>(List.of(1,2,3,4,5,7))));
+
+
+        ArrayList<Ranking> rankingList = user.getRankingList(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
+
+
+        FinalFirstPlaceCalculator finalFirstPlaceCalculator = new FinalFirstPlaceCalculator();
+
+        for (Ranking ranking : rankingList) {
+            if(ranking == Ranking.FIRST) {
+                finalFirstPlaceCalculator.add(new FinalFirstCandidate(user));
+            }
+
+            else if(ranking == Ranking.SECOND) {
+                user.increaseSecondPlaceWinningStack();
+            }
+
+        }
+
+
+        //second place calculate -> need test
+        int secondPlaceWinningStack = user.getSecondPlaceWinningStack();
+        int share = secondPlaceWinningStack / secondPlaceWinningStackSize;
+        int remainder = secondPlaceWinningStack % secondPlaceWinningStackSize;
+
+
+        for (int i = 0; i < share; i++) {
+            finalFirstPlaceCalculator.add(new FinalFirstCandidate(user));
+        }
+
+        user.setSecondPlaceWinningStack(remainder);
+
+        Assertions.assertThat(user.getSecondPlaceWinningStack()).isEqualTo(2);
     }
 
 }
