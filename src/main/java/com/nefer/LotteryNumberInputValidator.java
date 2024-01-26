8df70b2df2fd34d733e43ae398bf075eeef57fcf
cheolhakja <rebegin15@gmail.com>
2024-01-27 00:38:01 +0900

    중복된 로또번호가 있는지 검사하는 기능을 추가해야 한다

---
+++ src/main/java/com/nefer/LotteryNumberInputValidator.java
@@ -0,0 +1,15 @@
+package com.nefer;
+
+import java.util.ArrayList;
+
+public class LotteryNumberInputValidator {
+
+    /*
+    1. 로또 번호에 중복이 없는지 검사하는 클래스
+    2. DropdownController 에서 LotteryNumberInputValidator 객체를 만든 뒤 유효성 체크
+     */
+
+    public boolean checkForDuplicateLotteryNumbers(ArrayList<Integer> lotteryNumbers) {
+        return true;
+    }
+}
