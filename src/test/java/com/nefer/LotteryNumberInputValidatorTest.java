3e31e7d8e99bda69a981f2676b516c32338b61eb
조용준 <whdydwns0604@gmail.com>
2024-01-29 15:48:15 +0900

    Test : LotteryNumberInputValidator

---
+++ src/test/java/com/nefer/LotteryNumberInputValidatorTest.java
@@ -0,0 +1,20 @@
+package com.nefer;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LotteryNumberInputValidatorTest {
+    @Test
+    public void test(){
+        LotteryNumberInputValidator lotteryNumberInputValidator = new LotteryNumberInputValidator();
+        ArrayList<Integer> integers1 = new ArrayList<>(List.of(1,1,3,4,5,6));
+        ArrayList<Integer> integers2 = new ArrayList<>(List.of(1,2,3,4,5,6));
+        Assertions.assertThat(lotteryNumberInputValidator.checkForDuplicateLotteryNumbers(integers1)).isEqualTo(false);
+        Assertions.assertThat(lotteryNumberInputValidator.checkForDuplicateLotteryNumbers(integers2)).isEqualTo(true);
+
+
+    }
+}
